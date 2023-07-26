c26839ab30803e92ff25ac49eb5cb7930d79c262
hsoubecommerce <hsoubecommerce@gmail.com>
2023-07-26 17:33:44 +0200

    faetured product done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/style.css';
 import '@fortawesome/fontawesome-free/js/all.min.js'
 
-document.querySelectorAll('[data-bs-toggle="tooltip"]').map(item=>new bootstrap.Tooltip(item))
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item=>new bootstrap.Tooltip(item))
 
 console.log("اهلا بك ف متجر بالعربي")
