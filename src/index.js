035d62257b50576afdf400a8ac23bf01d0b757d0
hsoubecommerce <hsoubecommerce@gmail.com>
2023-08-03 20:19:48 +0200

    carousel-most-sold

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@ import '@fortawesome/fontawesome-free/js/all.min.js'
 
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item=>new bootstrap.Tooltip(item))
 
+document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+    item.addEventListener("click", () => {
+    alert('!اضيف هذا المنتج الي عربه الشراء');
+    })
+});
 console.log("اهلا بك ف متجر بالعربي")
